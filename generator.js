bdf95d300beee1b08c192617e871b4509abc3325
Jani <aijala.j@gmail.com>
2023-08-02 14:59:40 +0300

    created generator.js and added four arrays with words.

---
+++ generator.js
@@ -0,0 +1,36 @@
+const wordsGroup1 = ['Did you know that', 'Can you belive that'];
+const SecondWordsGroup1 = [
+    'Scotland chose the unicorn as its national animal.', 
+    'Avocados are not vegetables.',
+    "Shrimp's heart is in its head.",
+    'Sloths are pretty good in water. They can even hold their breath longer than dolphins.',
+    'First airplane flought on 17.12.1903',
+    'Venus spins clockwise.',
+    'The american Statue of Liberty was once upon a time used as a lighthouse',
+    'Octopuses have three hearts'
+];
+
+const wordsGroup2 = [
+    'I think we should',
+    'I think we should not',
+    'Do you think we should',
+    'I love you', //only together with second to last from secondWordsGroup2
+    'All your base are' //only together with the last from secondwordsGroup2
+];
+
+const secondWordsGroup2 = [
+    'have a date',
+    'go to cinema',
+    'go to our separate ways from here',
+    'go and say hi to John',
+    'log off',
+    'go to sleep',
+    'make life more fun',
+    'take what is rightfully ours',
+    'take this base',
+    '... not you! the one behind you!', //only together with the second to last from wordsGroup2
+    'belong to us!' //only together with the last from wordsGroup2
+];
+
+console.log(wordsGroup1);
+
