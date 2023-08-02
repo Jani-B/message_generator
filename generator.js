282e6549c1f6f77173da99e2192f3f577d616394
Jani <aijala.j@gmail.com>
2023-08-02 16:01:23 +0300

    created working version of the generator

--- generator.js
+++ generator.js
@@ -37,22 +37,43 @@ const chooseGroup = () => { //create a random number between 0-1 to see which gr
     return randomNumber
 };
 
+//console.log(chooseGroup());
+
 let randomSentence1 = [];
-const randomForGroup1 = () => { //random generator for the wordsGroup1
-    let randomNumber = Math.floor(Math.random() * wordsGroup1.length);
-    let randomWord1 = wordsGroup1[randomNumber];
-    let randomNumber2 = Math.floor(Math.random() * secondWordsGroup1.length);
-    let randomWord2 = secondWordsGroup1[randomNumber2];
-    randomSentence1.push(randomWord1, randomWord2);
-    return randomSentence1
+const createSentence = () => { //random generator for the wordsGroup1
+    if(chooseGroup() === 0) {
+        let randomNumber = Math.floor(Math.random() * wordsGroup1.length);
+        let randomWord1 = wordsGroup1[randomNumber];
+        let randomNumber2 = Math.floor(Math.random() * secondWordsGroup1.length);
+        let randomWord2 = secondWordsGroup1[randomNumber2];
+        randomSentence1.push(randomWord1, randomWord2);
+        return randomSentence1
+    }else {
+        let randomNumber = Math.floor(Math.random() * wordsGroup2.length);
+        let randomWord1 = wordsGroup2[randomNumber];
+            if (randomWord1 === 'I love you'){
+                let chooseWord = secondWordsGroup2[9];
+                randomSentence1.push(randomWord1, chooseWord);
+                return randomSentence1
+            } else if (randomWord1 === 'All your base are'){
+                let chooseWord = secondWordsGroup2[10];
+                randomSentence1.push(randomWord1, chooseWord);
+                return randomSentence1
+            } else {
+        let randomNumber2 = Math.floor(Math.random() * (secondWordsGroup2.length - 2));
+        let randomWord2 = secondWordsGroup2[randomNumber2];
+        randomSentence1.push(randomWord1, randomWord2);
+        return randomSentence1
+        }
+    };
 };
 
 const createText = () => {
-    console.log(`${randomForGroup1()}`);
+    console.log(`Random Message for you:   ${randomSentence1.join(' ')}`);
 };
 
-createText();
-
+createText(createSentence());
+//console.log(randomSentence1);
 //console.log(wordsGroup1);
 //console.log(randomForGroup1());
 
